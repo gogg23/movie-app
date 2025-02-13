379559ccc07106d9ebd1700cc16c7e846cea85fd
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-02-13 15:42:35 +0300

    commit

---
+++ .history/movie app/src/components/MovieCard_20250212181348.jsx
@@ -0,0 +1,17 @@
+
+
+function MovieCard({ movie }) {
+
+    function onFavoriteClick () {
+        alert("clicked")
+    }
+    
+  return <div className='movie-card'>
+      <div className='movie-poster'>
+        <img src={movie.url} alt={'movie.title'} />
+        <div className="movie-overlay"></div>
+        <button className="favorite-btn" onClick={}></button>
+      </div>
+    </div>
+
+}
