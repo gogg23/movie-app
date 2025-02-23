8c3e717e26dc05e166b8e57f4526aa8571d63def
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-02-23 20:54:30 +0300

    commit

---
+++ .history/movie app/src/pages/Home_20250223205227.jsx
@@ -0,0 +1,53 @@
+import MovieCard from '../components/MovieCard';
+import { useState, useEffect } from 'react';
+import { searchMovies, getPopularMovies } from '../services/api';
+import '../css/Home.css';
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('null');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError('failed to load movies...');
+      } finally {
+      }
+    };
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault(e);
+    alert(searchQuery);
+    setSearchQuery('');
+  };
+
+  return (
+    <div className='home'>
+      <form onSubmit={handleSearch} className='search-form'>
+        <input
+          type='text'
+          placeholder='search for movies...'
+          className='search-input'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type='submit' className='search-button'>
+          Search
+        </button>
+      </form>
+      <div className='movies-grid'>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Home;
